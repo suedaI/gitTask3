8fd1d6b1c8ad190d72f727e74a9e9bd59ac7603e
suedaislak <suedakinci@gmail.com>
2022-01-11 21:22:56 -0500

    deneme1

--- src/deneme2/deneme4.java
+++ src/deneme2/deneme4.java
@@ -3,5 +3,7 @@ package deneme2;
 public class deneme4 {
     public static void main(String[] args) {
         System.out.println("00002");
+        System.out.println("123");
+        System.out.println("567");
     }
 }
